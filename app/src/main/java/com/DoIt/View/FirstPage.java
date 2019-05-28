e18be12acb89b41874c64e886dfbbebba735ff65
LancetVyhb  email = 923512461@qq.com <44900729+LancetVyhb@users.noreply.github.com>
2019-05-28 11:39:14 +0800

    优化
    优化

--- app/src/main/java/com/DoIt/View/FirstPage.java
+++ app/src/main/java/com/DoIt/View/FirstPage.java
@@ -38,7 +38,7 @@ public class FirstPage extends AppCompatActivity {
             Manifest.permission.RECEIVE_BOOT_COMPLETED,
     };
     public static final Integer VERSION_I = 7;
-    public static final String CHANNEL = "huawei";
+    public static final String CHANNEL = "baidu";
     private PermissionPageUtils utils;
     private Progress progress;
 
